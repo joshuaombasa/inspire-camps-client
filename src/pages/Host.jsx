b17f9121d5fdc0139ee37befe25ca100be23c1e1
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-10-01 23:50:11 +0300

    added host page

---
+++ src/pages/Host.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Host () {
+    return (
+        <div className="host--page">
+            <h1>This is host page.</h1>
+        </div>
+    )
+}
