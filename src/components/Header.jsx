60231cded17a185732ef3ae0f50dfe4d8e23c525
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-10-01 23:57:54 +0300

    added header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+import { Link, NavLink } from "react-router-dom";
+
+export default function Header() {
+    return (
+        <header>
+            <Link className="logo">inspireCamps</Link>
+            <nav>
+                <NavLink 
+                     to="/host"
+                     className={({isActive}) => isActive === true ? "active--style" : ""}
+                     >Host</NavLink>
+                <NavLink 
+                     to="/camps"
+                     className={({isActive}) => isActive === true ? "active--style" : ""}
+                     >Camps</NavLink>
+                <NavLink 
+                     to="/about"
+                     className={({isActive}) => isActive === true ? "active--style" : ""}
+                     >About</NavLink>
+                <NavLink 
+                     to="/login"
+                     className={({isActive}) => isActive === true ? "active--style" : ""}
+                     >Login</NavLink>
+            </nav>
+        </header>
+    )
+}
+
+// active--style
