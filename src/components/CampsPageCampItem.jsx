ebeee3aab07b97f6a1a975972abd6239d6369b9a
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-10-02 19:27:12 +0300

    updated styling

--- src/components/CampsPageCampItem.jsx
+++ src/components/CampsPageCampItem.jsx
@@ -1,15 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
-export default function CampsPageCampItem({ camp }) {
+export default function CampsPageCampItem({ camp, typeFilter, search }) {
+
+    const styles = {
+        backgroundColor : camp.type === "simple" ? "#115E59"  :  camp.type === "luxury" ? "goldenrod" : "#FFCC8D"
+    }
+
     return (
         <div className="CampsPageCampItem">
-            <Link to={camp.id}>
+            <Link to={camp.id} state={{typeFilter, search}}>
                 <img src={camp.imageUrl} alt="" />
                 <div className="details">
                     <h3>{camp.name}</h3>
                     <h4>${camp.price}/day</h4>
                 </div>
-                <span>{camp.type}</span>
+                <span style={styles}>{camp.type}</span>
             </Link>
         </div>
     )
