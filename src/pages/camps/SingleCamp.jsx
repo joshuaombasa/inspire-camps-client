4248bd129bb3a54a2d7a5c77b7f84a1d0a857e65
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-10-02 19:32:09 +0300

    added camps filter logic logic state prop

--- src/pages/camps/SingleCamp.jsx
+++ src/pages/camps/SingleCamp.jsx
@@ -1,10 +1,13 @@
 import React, { useState } from "react";
-import { useParams, Link } from "react-router-dom";
+import { useParams, Link, useLocation } from "react-router-dom";
 export default function SingleCamp() {
 
     const { id } = useParams()
 
     const [currentCamp, setCurrentCamp] = useState(null)
+
+    const location = useLocation()
+    // console.log(location.state)
 
     React.useEffect(() => {
         fetch(`http://localhost:3000/api/camps/${id}`)
@@ -14,8 +17,17 @@ export default function SingleCamp() {
             })
     }, [])
 
+    const path = location.state.search ?  `?${location.state.search}` : ""
+
+    const backText = location.state.typeFilter ? location.state.typeFilter : "all"
+
+
     if (!currentCamp) {
         return <h1>Loading...</h1>
+    }
+
+    const styles = {
+        backgroundColor : currentCamp.type === "simple" ? "#115E59"  :  currentCamp.type === "luxury" ? "goldenrod" : "#FFCC8D"
     }
 
 
@@ -25,11 +37,11 @@ export default function SingleCamp() {
             <Link
                 className="back--link"
                 relative="path"
-                to=".."
-            >&larr; <span className="back--link--text">Back to all camps</span></Link>
+                to={`..${path}`}
+            >&larr; <span className="back--link--text">Back to {backText} camps</span></Link>
             <div className="single--camp--page--container">
                 <img src={currentCamp.imageUrl} alt="" />
-                <span>{currentCamp.type}</span>
+                <span style={styles}>{currentCamp.type}</span>
                 <h1>{currentCamp.name}</h1>
                 <h3>${currentCamp.price}/day</h3>
                 <p>{currentCamp.description}</p>
