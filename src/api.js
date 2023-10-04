37ae86f25ecc728b6c846ed894ffca30d8434ad8
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-10-04 23:13:43 +0300

    updated error handling in fetching logic

--- src/api.js
+++ src/api.js
@@ -3,14 +3,12 @@
 export async function getCamps() {
     const res = await  fetch("http://localhost:3000/api/camps")
     if (!res.ok) {
-        throw console.error( {
-            mesage : "Failed to fetch",
-            statusText : error.statusText,
-            status : error.status
-        });
+        throw {
+            message : "Failed to fetch",
+            statusText : res.statusText,
+            status : res.status
+        };
     }
-
-    console.log(res)
 
     const data = await res.json()
     return data
@@ -18,18 +16,42 @@ export async function getCamps() {
 
 export async function getSingleCamp(id) {
     const res = await  fetch(`http://localhost:3000/api/camps/${id}`)
+    if (!res.ok) {
+        throw {
+            message : "Failed to fetch",
+            statusText : res.statusText,
+            status : res.status
+        };
+    }
+
     const data = await res.json()
     return data
 }
 
 export async function getHostCamps() {
     const res = await fetch("http://localhost:3000/api/host/camps")
+    if (!res.ok) {
+        throw {
+            message : "Failed to fetch",
+            statusText : res.statusText,
+            status : res.status
+        };
+    }
+
     const data = await res.json()
     return data
 }
 
 export async function getSingleHostCamp(id) {
     const res = await  fetch(`http://localhost:3000/api/camps/${id}`)
+    if (!res.ok) {
+        throw {
+            message : "Failed to fetch",
+            statusText : res.statusText,
+            status : res.status
+        };
+    }
+
     const data = await res.json()
     return data
 }
