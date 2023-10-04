599c2d94328d8c80003362444ec5cfd525748b0c
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-10-04 11:19:23 +0300

    updated component logic

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,52 @@
 import React from "react";
 
-export default function Login () {
+export default function Login() {
+
+    const [formData, setFormData] = React.useState({
+        email: "",
+        password: "",
+    })
+
+    function handleChange(event) {
+        const { name, value } = event.target
+        setFormData(prevFormData => (
+            {
+                ...prevFormData,
+                [name]: value
+            }
+        ))
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault()
+        console.log(formData)
+    }
+
     return (
-        <div className="Login--page">
-            <h1>This is Login page.</h1>
+        <div className="login--page">
+            <div className="login--page--container">
+                <h2>Sign in to your account</h2>
+                <form onSubmit={handleSubmit} className="login--form">
+                    <input
+                        className="input--top"
+                        type="email"
+                        placeholder="Email address"
+                        name="email"
+                        value={formData.email}
+                        onChange={handleChange}
+                    />
+                    <input
+                        type="password"
+                        className="input--bottom"
+                        name="password"
+                        placeholder="Password"
+                        value={formData.password}
+                        onChange={handleChange}
+                    />
+                    <button>Sign in</button>
+                </form>
+                <p>Dont have an account? <span>Create one now</span></p>
+            </div>
         </div>
     )
 }
