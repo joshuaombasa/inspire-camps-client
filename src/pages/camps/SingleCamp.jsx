8788e43a1f5af8f357742bd56f52d4744006c822
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-10-05 22:29:29 +0300

    updated component to use loaders for data fetching

--- src/pages/camps/SingleCamp.jsx
+++ src/pages/camps/SingleCamp.jsx
@@ -1,41 +1,40 @@
 import React, { useState } from "react";
-import { useParams, Link, useLocation } from "react-router-dom";
+import { useParams, Link, useLocation, useLoaderData } from "react-router-dom";
 import { getSingleCamp } from "../../api";
 
+export function loader({params}) {
+    
+    return getSingleCamp(params.id)
+}
+
 export default function SingleCamp() {
-
-    const { id } = useParams()
-
-    const [currentCamp, setCurrentCamp] = useState(null)
-    const [loading, setLoading] = useState(true)
     const [error, setError] = useState(null)
+
+    const currentCamp = useLoaderData()
 
     const location = useLocation()
     // console.log(location.state)
 
-    React.useEffect(() => {
-        async function fetchSingleCamp() {
-            setLoading(true)
-            try {
-                const data = await getSingleCamp(id)
-                setCurrentCamp(data)
-            } catch (error) {
-                setError(error)
-            } finally {
-                setLoading(false)
-            }
-        }
-        fetchSingleCamp()
-    }, [])
+    // React.useEffect(() => {
+    //     async function fetchSingleCamp() {
+    //         setLoading(true)
+    //         try {
+    //             const data = await getSingleCamp(id)
+    //             setCurrentCamp(data)
+    //         } catch (error) {
+    //             setError(error)
+    //         } finally {
+    //             setLoading(false)
+    //         }
+    //     }
+    //     fetchSingleCamp()
+    // }, [])
 
     const path = location.state.search ? `?${location.state.search}` : ""
 
     const backText = location.state.typeFilter ? location.state.typeFilter : "all"
 
 
-    if (loading) {
-        return <h1>Loading...</h1>
-    }
 
     if (error) {
         return (
