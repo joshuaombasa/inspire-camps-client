c440b90c617687f36264516023b58213ea5c5a94
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-10-05 21:59:59 +0300

    added loader props to camps and hostcamps routes

--- src/App.jsx
+++ src/App.jsx
@@ -10,12 +10,12 @@ import Layout from './components/Layout'
 import Home from './pages/Home'
 import About from './pages/About'
 import Host from './pages/Host'
-import Camps from './pages/camps/Camps'
+import Camps, { loader as campsLoader } from './pages/camps/Camps'
 import Login from './pages/Login'
 import SingleCamp from './pages/camps/SingleCamp'
 import HostLayout from './components/HostLayout'
 import Dashboard from './pages/host/Dashboard'
-import HostCamps from './pages/host/HostCamps'
+import HostCamps, {loader as hostCampsLoader} from './pages/host/HostCamps'
 import Income from './pages/host/Income'
 import Reviews from './pages/host/Reviews'
 import HostCampDetails from './pages/host/HostCampDetails'
@@ -34,7 +34,7 @@ function App() {
       <Route path='host' element={<HostLayout />}>
         <Route index element={<Dashboard />} />
         <Route path='income' element={<Income />} />
-        <Route path='camps' element={<HostCamps />} />
+        <Route path='camps' element={<HostCamps />} loader={hostCampsLoader}/>
         <Route path='camps/:id' element={<HostCampDetails />} >
           <Route index element={<SelectedCampDetails />} />
           <Route path='pricing' element={<SelectedCampPrice />} />
@@ -42,7 +42,7 @@ function App() {
         </Route>
         <Route path='reviews' element={<Reviews />} />
       </Route>
-      <Route path='camps' element={<Camps />} />
+      <Route path='camps' element={<Camps />} loader={campsLoader}/>
       <Route path='camps/:id' element={<SingleCamp />} />
       <Route path='login' element={<Login />} />
       <Route path='*' element={<NotFound/>}/>
