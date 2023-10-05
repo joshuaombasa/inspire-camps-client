8e039e6bcce6bf1fc69adf9b7da4c5385a084745
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-10-05 21:59:20 +0300

    updated component to use loaders for data fetching

--- src/pages/host/HostCamps.jsx
+++ src/pages/host/HostCamps.jsx
@@ -1,27 +1,31 @@
 import React, {useState} from "react";
-import { Link } from "react-router-dom";
+import { Link, useLoaderData} from "react-router-dom";
 import HostCampItem from "../../components/HostCampItem";
 import { getHostCamps } from "../../api";
 
+export function loader() {
+    return getHostCamps()
+}
+
 export default function HostCamps() {
-    const [hostCamps, setHostCamps] = React.useState([])
+    const hostCamps = useLoaderData()
     const [loading, setLoading] = useState(false)
     const [error, setError] = useState(null)
 
-    React.useEffect(() => {
-        async function fetchHostCamps() {
-            setLoading(true)
-            try {
-                const data = await getHostCamps()
-                setHostCamps(data)
-            } catch (error) {
-                setError(error)
-            } finally {
-                setLoading(false)
-            }
-        }
-        fetchHostCamps()
-    }, [])
+    // React.useEffect(() => {
+    //     async function fetchHostCamps() {
+    //         setLoading(true)
+    //         try {
+    //             const data = await getHostCamps()
+    //             setHostCamps(data)
+    //         } catch (error) {
+    //             setError(error)
+    //         } finally {
+    //             setLoading(false)
+    //         }
+    //     }
+    //     fetchHostCamps()
+    // }, [])
 
     if (loading) {
         return (
