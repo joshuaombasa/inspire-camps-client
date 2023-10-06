585b72c6526eac473d3cb8db1acd11dad9198da4
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-10-06 23:56:12 +0300

    added authentication logic on component loader

--- src/pages/host/Reviews.jsx
+++ src/pages/host/Reviews.jsx
@@ -1,5 +1,11 @@
 import React from "react";
 
+import { authRequired } from "../../utils";
+
+export async function loader() {
+    await authRequired()
+}
+
 export default function Reviews() {
     return (
         <h1>This is reviews</h1>
