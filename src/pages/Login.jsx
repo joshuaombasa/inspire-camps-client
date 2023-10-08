cbbfc72af70052b56985be1e4e2ec5164790ebec
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-10-08 23:26:47 +0300

    added dynamic disabled state to log in button

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,20 +1,20 @@
 import React from "react";
-import { useNavigate, useSearchParams, Form, useActionData, redirect } from "react-router-dom";
+import { useSearchParams, Form, useActionData, redirect, useNavigation } from "react-router-dom";
 import { loginUser } from "../api";
 
-export function loader({request}) {
-   return null
+export function loader({ request }) {
+    return null
 
 }
 
-export async function action({request}) {
+export async function action({ request }) {
     const formData = await request.formData()
     const email = formData.get("email")
     const password = formData.get("password")
-    console.log({email, password})
+    console.log({ email, password })
 
     try {
-        const data = await loginUser({email, password})
+        const data = await loginUser({ email, password })
         console.log(data)
         throw redirect("/host")
     } catch (error) {
@@ -35,7 +35,8 @@ export default function Login() {
     const [searchParams, setSearchParams] = useSearchParams()
     const message = searchParams.get("message")
 
-    const navigate = useNavigate()
+    const navigate = useNavigation()
+    console.log(navigate.state)
 
     function handleChange(event) {
         const { name, value } = event.target
@@ -54,7 +55,7 @@ export default function Login() {
             try {
                 const data = await loginUser(formData)
                 console.log(data)
-                navigate("/host", {replace: true})
+                navigate("/host", { replace: true })
             } catch (error) {
                 setError(error)
             }
@@ -68,7 +69,7 @@ export default function Login() {
                 <h2>Sign in to your account</h2>
                 {message && <h3 className="red">{message}</h3>}
                 {error && <h4 className="red">{error.message}</h4>}
-                <Form method="post"  className="login--form">
+                <Form method="post" className="login--form">
                     <input
                         className="input--top"
                         type="email"
@@ -85,7 +86,9 @@ export default function Login() {
                         value={formData.password}
                         onChange={handleChange}
                     />
-                    <button>Sign in</button>
+                    <button disabled={navigate.state === "submitting"}>
+                        {navigate.state === "submitting" ? "Logging in.." : "Log in"}
+                    </button>
                 </Form >
                 <p>Dont have an account? <span>Create one now</span></p>
             </div>
