0c8bbd57e20d25cade63653987ed1de5fa92532d
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-10-08 22:54:01 +0300

    added search string to the redirect parameter

--- src/utils.js
+++ src/utils.js
@@ -1,9 +1,9 @@
 import { redirect } from "react-router-dom"
 
 export async function authRequired() {
-    const isLoggedIn = false
+    const isLoggedIn = true
 
     if (!isLoggedIn) {
-        throw redirect("/login")
+        throw redirect("/login?message=you must login first")
     }
 }
