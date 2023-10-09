b77a40853d8a9049dd1b3393ecfec9c9780233e6
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-10-09 21:23:08 +0300

    updated utulity to send redirectTo query string  to  login

--- src/utils.js
+++ src/utils.js
@@ -1,9 +1,12 @@
 import { redirect } from "react-router-dom"
 
-export async function authRequired() {
-    const isLoggedIn = true
+export async function authRequired(request) {
+    const isLoggedIn = localStorage.getItem("loggedIn")
+    
+    const url = new URL(request.url)
+    const pathname = url.pathname
 
     if (!isLoggedIn) {
-        throw redirect("/login?message=you must login first")
+        throw redirect(`/login?message=you must login first&redirectTo=${pathname}`)
     }
 }
