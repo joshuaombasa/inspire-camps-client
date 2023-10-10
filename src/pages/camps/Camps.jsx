5146ee8484c1fa778719874642b9c9c20177c7fc
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-10-10 21:15:06 +0300

    updated component to display a  fallback until its children have finished loading

--- src/pages/camps/Camps.jsx
+++ src/pages/camps/Camps.jsx
@@ -1,17 +1,17 @@
-import React, { useState } from "react";
+import React, { Suspense, useState } from "react";
 import CampsPageCampItem from "../../components/CampsPageCampItem";
-import { useSearchParams, useLoaderData } from "react-router-dom";
+import { useSearchParams, useLoaderData, defer, Await } from "react-router-dom";
 import { getCamps } from "../../api";
 
 export function loader() {
-    return getCamps()
+    return defer({ camps: getCamps() })
 }
 
 export default function Camps() {
     const [loading, setLoading] = useState(false)
     const [error, setError] = useState(null)
-    const camps = useLoaderData()
- 
+    const dataPromise = useLoaderData()
+
 
     const [searchParams, setSearchParams] = useSearchParams()
 
@@ -21,7 +21,7 @@ export default function Camps() {
     //         try {
     //             const data = await getCamps()
     //             setCamps(data)
-               
+
     //         } catch (error) {
     //             setError(error)
     //         } finally {
@@ -32,7 +32,7 @@ export default function Camps() {
     //     fetchCamps()
     // }, [])
 
- 
+
 
     if (error) {
         return (
@@ -53,37 +53,52 @@ export default function Camps() {
 
     const typeFilter = searchParams.get("type")
 
-    const filteredCamps = typeFilter
-        ? camps.filter(camp => camp.type === typeFilter)
-        : camps
+    function renderCamps(camps) {
+        const filteredCamps = typeFilter
+            ? camps.filter(camp => camp.type === typeFilter)
+            : camps
 
-    const imgElements = filteredCamps.map(camp => (
-        <CampsPageCampItem
-            key={camp.id}
-            camp={camp}
-            typeFilter={typeFilter}
-            search={searchParams.toString()}
-        />
-    ))
+        const imgElements = filteredCamps.map(camp => (
+            <CampsPageCampItem
+                key={camp.id}
+                camp={camp}
+                typeFilter={typeFilter}
+                search={searchParams.toString()}
+            />
+        ))
+
+        return (
+            <>
+                <nav className="camps--filter">
+                    <button
+                        onClick={() => generateNewSerchParams("type", "simple")}
+                    >Simple</button>
+                    <button
+                        onClick={() => generateNewSerchParams("type", "luxury")}
+                    >Luxury</button>
+                    <button
+                        onClick={() => generateNewSerchParams("type", "rustic")}
+                    >rustic</button>
+                    <button
+                        onClick={() => generateNewSerchParams("type", null)}
+                    >Clear filter</button>
+                </nav>
+                <div className="camps--page--container">
+                    {imgElements}
+                </div>
+            </>
+        )
+    }
+
+
     return (
         <div className="camps--page">
-            <nav className="camps--filter">
-                <button
-                    onClick={() => generateNewSerchParams("type", "simple")}
-                >Simple</button>
-                <button
-                    onClick={() => generateNewSerchParams("type", "luxury")}
-                >Luxury</button>
-                <button
-                    onClick={() => generateNewSerchParams("type", "rustic")}
-                >rustic</button>
-                <button
-                    onClick={() => generateNewSerchParams("type", null)}
-                >Clear filter</button>
-            </nav>
-            <div className="camps--page--container">
-                {imgElements}
-            </div>
+            <Suspense fallback={<h1>Loading</h1>}>
+            <Await resolve={dataPromise.camps}>
+                { renderCamps }
+            </Await>
+            </Suspense>
+
         </div>
     )
 }
