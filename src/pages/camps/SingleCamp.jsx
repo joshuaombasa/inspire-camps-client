961f1d6d8ef139df004b7873d28c15302277de9a
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-10-10 21:15:17 +0300

    updated component to display a  fallback until its children have finished loading

--- src/pages/camps/SingleCamp.jsx
+++ src/pages/camps/SingleCamp.jsx
@@ -1,16 +1,15 @@
-import React, { useState } from "react";
-import { useParams, Link, useLocation, useLoaderData } from "react-router-dom";
+import React, { useState, Suspense } from "react";
+import { useParams, Link, useLocation, useLoaderData, defer, Await } from "react-router-dom";
 import { getSingleCamp } from "../../api";
 
-export function loader({params}) {
-    
-    return getSingleCamp(params.id)
+export function loader({ params }) {
+    return defer({ camp: getSingleCamp(params.id) })
 }
 
 export default function SingleCamp() {
     const [error, setError] = useState(null)
 
-    const currentCamp = useLoaderData()
+    const dataPromise = useLoaderData()
 
     const location = useLocation()
 
@@ -41,27 +40,38 @@ export default function SingleCamp() {
         )
     }
 
-    const styles = {
-        backgroundColor: currentCamp.type === "simple" ? "#115E59" : currentCamp.type === "luxury" ? "goldenrod" : "#FFCC8D"
+
+
+    function renderSingleCamp(currentCamp) {
+        const styles = {
+            backgroundColor: currentCamp.type === "simple" ? "#115E59" : currentCamp.type === "luxury" ? "goldenrod" : "#FFCC8D"
+        }
+        return (
+            <>
+                <Link
+                    className="back--link"
+                    relative="path"
+                    to={`..${path}`}
+                >&larr; <span className="back--link--text">Back to {backText} camps</span></Link>
+                <div className="single--camp--page--container">
+                    <img src={currentCamp.imageUrl} alt="" />
+                    <span style={styles}>{currentCamp.type}</span>
+                    <h1>{currentCamp.name}</h1>
+                    <h3>${currentCamp.price}/day</h3>
+                    <p>{currentCamp.description}</p>
+                    <Link>Rent this camp</Link>
+                </div>
+            </>
+        )
     }
-
-
 
     return (
         <div className="single--camp--page">
-            <Link
-                className="back--link"
-                relative="path"
-                to={`..${path}`}
-            >&larr; <span className="back--link--text">Back to {backText} camps</span></Link>
-            <div className="single--camp--page--container">
-                <img src={currentCamp.imageUrl} alt="" />
-                <span style={styles}>{currentCamp.type}</span>
-                <h1>{currentCamp.name}</h1>
-                <h3>${currentCamp.price}/day</h3>
-                <p>{currentCamp.description}</p>
-                <Link>Rent this camp</Link>
-            </div>
+            <Suspense fallback={<h1>Loading...</h1>}>
+                <Await resolve={dataPromise.camp}>
+                    {renderSingleCamp}
+                </Await>
+            </Suspense>
         </div>
     )
 }
