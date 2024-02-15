4260fafe2b296a646de70753246e4827c62621a6
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-02-15 23:10:53 +0300

    add data-testid attribute for testint

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ export default function Home() {
         <div className="home--page">
             <div className="home--page--container">
                 <h1>Escape. Explore. Embrace. Welcome to Inspirecamps</h1>
-                <p>Unplug from the digital world and reconnect with the great outdoors. Whether you're an experienced camper or a first-time adventurer, our campsite provides the ideal backdrop for creating lasting memories. Explore lush forests, serene lakes, and hiking trails that lead to stunning vistas. Roam free and awaken your inner explorer.</p>
+                <p data-testid="homeParagraph">Unplug from the digital world and reconnect with the great outdoors. Whether you're an experienced camper or a first-time adventurer, our campsite provides the ideal backdrop for creating lasting memories. Explore lush forests, serene lakes, and hiking trails that lead to stunning vistas. Roam free and awaken your inner explorer.</p>
                 <Link to="/camps">Find your camp</Link>
             </div>
         </div>
